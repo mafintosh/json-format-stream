64301a293b7a067fd278286fa9446e3d30ebee3c
Mathias Buus <mathiasbuus@gmail.com>
2015-02-03 14:00:04 +0100

    add tests

---
+++ test.js
@@ -0,0 +1,54 @@
+var tape = require('tape')
+var concat = require('concat-stream')
+var format = require('./')
+
+tape('success response', function (t) {
+  var stream = format()
+
+  stream.write({hello: 'world'})
+  stream.write({hello: 'new world'})
+  stream.end()
+
+  stream.pipe(concat(function (data) {
+    t.same(JSON.parse(data), {result: [{hello: 'world'}, {hello: 'new world'}], error: null})
+    t.end()
+  }))
+})
+
+tape('error response', function (t) {
+  var stream = format()
+
+  stream.write({hello: 'world'})
+  stream.write({hello: 'new world'})
+  stream.destroy(new Error(':('))
+
+  stream.pipe(concat(function (data) {
+    t.same(JSON.parse(data), {result: [{hello: 'world'}, {hello: 'new world'}], error: ':('})
+    t.end()
+  }))
+})
+
+tape('close error response', function (t) {
+  var stream = format()
+
+  stream.write({hello: 'world'})
+  stream.write({hello: 'new world'})
+  stream.destroy()
+
+  stream.pipe(concat(function (data) {
+    t.same(JSON.parse(data), {result: [{hello: 'world'}, {hello: 'new world'}], error: 'Unexpected close of stream'})
+    t.end()
+  }))
+})
+
+tape('metadata', function (t) {
+  var stream = format({metadata: 42, test: 10})
+
+  stream.write({hello: 'world'})
+  stream.end()
+
+  stream.pipe(concat(function (data) {
+    t.same(JSON.parse(data), {metadata: 42, test: 10, result: [{hello: 'world'}], error: null})
+    t.end()
+  }))
+})
